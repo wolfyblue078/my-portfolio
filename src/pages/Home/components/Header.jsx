9f3feaada5568af0249568670edd080550a18f7d
wolfyblue078 <wolfyblue078@gmail.com>
2026-04-06 12:58:32 +0530

    feat: optimize homepage logo for multiple screens ✅✅

--- src/pages/Home/components/Header.jsx
+++ src/pages/Home/components/Header.jsx
@@ -7,7 +7,13 @@ export default function Header(){
 
         <div className="w-full bg-transparent">
             <header className="max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-8 md:px-7 py-4">
-                <span className="text-4xl font-bold text-white font-['Helvetica']">Portfolio</span>
+                
+                {/* Logo */}
+                <span className="text-2xl md:text-3xl font-bold text-white font-['Helvetica']">
+                    Portfolio
+                </span>
+
+                {/* Large screen Navigations */}
                 <nav className="hidden md:flex space-x-4 text-white font-['Helvetica'] text-xl flex gap-2">
 
                     <Link to="home" 
